f8d048ea6623c0bf8ff42d32346fe0f23fb60808
DmitryEvteshin <dmitry.evteshin@yandex.ru>
2025-05-24 14:52:47 +0400

    Готоов

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
+  let today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const yyyy = today.getFullYear();
+  today = dd + '.' + mm + '.' + yyyy;
 </script>
 
 <template>
+  <main>{{ today }}</main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
