fdba430ca0a0d9052631800ba131b8f752902f83
DmitryEvteshin <dmitry.evteshin@yandex.ru>
2025-05-29 18:44:49 +0400

    Готово

---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template>
+    <button class="button">
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+    .button {
+        width: 335px;
+        height: 68px;
+        top: 686px;
+        left: 531px;
+        border-radius: 100px;
+        padding: 16px 10px;
+        gap: 10px;
+        background: #008BFE;
+        font-family: Roboto;
+        font-weight: 400;
+        font-size: 24px;
+        line-height: 36px;
+        letter-spacing: 0%;
+        text-align: center;
+        border: none;
+        color: #FFFFFF;
+        ;
+    }
+</style>
+
